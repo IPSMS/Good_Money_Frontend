d62ab5a42968b3af5ad40950a1242edc43b338b7
Bobby BuffaloBoy <novenarii@gmail.com>
2020-08-31 00:44:31 -0400

    Updates

---
+++ precache-manifest.25c9d2721e800505875378c7b70178b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06189f7ac4bd1b18ea9867a014fc7cc",
+    "url": "/Good_Money_Frontend/index.html"
+  },
+  {
+    "revision": "c4f02ae011343708bb34",
+    "url": "/Good_Money_Frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "acddac54c4aac94c7fc6",
+    "url": "/Good_Money_Frontend/static/js/2.4ff08064.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Good_Money_Frontend/static/js/2.4ff08064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f02ae011343708bb34",
+    "url": "/Good_Money_Frontend/static/js/main.3ed0eb12.chunk.js"
+  },
+  {
+    "revision": "6d24c5ea4ac55cace219",
+    "url": "/Good_Money_Frontend/static/js/runtime-main.4e755464.js"
+  }
+]);
